25a5ad4b37bd83c68b520915555b2b51a3291ae7
Dmitriy <dimach3688@gmail.com>
2022-03-15 16:37:01 +0400

    Finished sprint 2 for Java

---
+++ Sprint2/09_LimitedQueue/src/Solution.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+    private static final List<Integer> queue = new ArrayList<>();
+    private static int queueLimit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int commandQuantity = Integer.parseInt(reader.readLine());
+        queueLimit = Integer.parseInt(reader.readLine());
+        StringTokenizer tokenizer;
+        String command;
+        for (int i = 0; i < commandQuantity; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            command = tokenizer.nextToken();
+            switch (command) {
+                case ("push") -> push(Integer.parseInt(tokenizer.nextToken()));
+                case ("pop") -> pop();
+                case ("peek") -> peek();
+                case ("size") -> size();
+            }
+        }
+    }
+
+    public static void push(int number) {
+        if (queue.size() < queueLimit) {
+            queue.add(0, number);
+        } else {
+            System.out.println("error");
+        }
+    }
+
+    public static void pop() {
+        if (queue.size() == 0) {
+            System.out.println("None");
+            return;
+        }
+        System.out.println(queue.get(getTopIndex()));
+        queue.remove(getTopIndex());
+    }
+
+    public static void peek() {
+        if (queue.size() == 0) {
+            System.out.println("None");
+            return;
+        }
+        System.out.println(queue.get(getTopIndex()));
+    }
+
+    public static void size() {
+        System.out.println(queue.size());
+    }
+
+    private static int getTopIndex() {
+        return queue.size() - 1;
+    }
+}
