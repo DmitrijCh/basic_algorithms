20e7f98e963517ec1b229364389666ee203e0811
Dmitriy <dimach3688@gmail.com>
2022-03-30 18:48:30 +0400

    Finished sprint 5 for Java

---
+++ Sprint5/05_SearchTree/src/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public static boolean treeSolution(Node head) {
+        return isBinTree(head, null, null);
+    }
+
+    public static boolean isBinTree(Node root, Node minNode, Node maxNode) {
+        if (root == null) {
+            return true;
+        }
+
+        if (minNode != null && root.value <= minNode.value) {
+            return false;
+        }
+
+        if (maxNode != null && root.value >= maxNode.value) {
+            return false;
+        }
+
+        return isBinTree(root.left, minNode, root) && isBinTree(root.right, root, maxNode);
+
+    }
+
+    private static void test() {
+        Node node1 = new Node(1, null, null);
+        Node node2 = new Node(4, null, null);
+        Node node3 = new Node(3, node1, node2);
+        Node node4 = new Node(8, null, null);
+        Node node5 = new Node(5, node3, node4);
+        assert treeSolution(node5);
+        node2.value = 5;
+        assert !treeSolution(node5);
+    }
+}
