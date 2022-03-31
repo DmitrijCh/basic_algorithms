fea491ba20328d277cb29a0beddae7e4a2878b66
Dmitriy <dimach3688@gmail.com>
2022-03-31 14:20:24 +0400

    Finished sprint 7 for Java

---
+++ Sprint7/04_FibonacciNumbers/src/Solution.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        List<Long> dp = new ArrayList<>();
+        dp.add(1L);
+        dp.add(1L);
+
+        long mod = 1_000_000_007;
+        for (int i = 2; i <= n; i++) {
+            dp.add((dp.get(i - 2) + dp.get(i - 1)) % mod);
+        }
+        System.out.println(dp.get(n));
+    }
+}
+
