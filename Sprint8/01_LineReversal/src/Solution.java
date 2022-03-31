54d87af47034615a981aa0aecc5927daf0898e33
Dmitriy <dimach3688@gmail.com>
2022-03-31 14:41:56 +0400

    Finished sprint 8 for Java

---
+++ Sprint8/01_LineReversal/src/Solution.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        while (stringTokenizer.hasMoreTokens()) {
+            result.insert(0, " ");
+            result.insert(0, stringTokenizer.nextToken());
+        }
+        System.out.println(result);
+    }
+}
